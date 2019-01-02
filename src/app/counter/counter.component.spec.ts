1485a6f17bb2309f863a7b45600811a4b462c4f9
Prabu <meetprabu88@gmail.com>
2019-01-02 18:45:18 +0530

    Adds test for counter

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -3,11 +3,12 @@ import { StoreModule, combineReducers, Store } from '@ngrx/store';
 
 import { CounterComponent } from './counter.component';
 import { counterReducer } from '../counter.reducer';
-
+import { Increment, Decrement, Reset } from '../counter.actions';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
   let fixture: ComponentFixture<CounterComponent>;
+  let store: Store<any>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,6 +16,8 @@ describe('CounterComponent', () => {
       declarations: [ CounterComponent ]
     })
     .compileComponents();
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
   }));
 
   beforeEach(() => {
@@ -26,4 +29,38 @@ describe('CounterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should increment counter', () => {
+    const action = new Increment();
+
+    component.increment();
+    expect(store.dispatch).toHaveBeenCalledWith(action);
+  });
+
+  it('should decrement counter', () => {
+    const action = new Decrement();
+
+    component.decrement();
+    expect(store.dispatch).toHaveBeenCalledWith(action);
+  });
+
+  it('should reset the counter', () => {
+    const action = new Reset();
+
+    component.reset();
+    expect(store.dispatch).toHaveBeenCalledWith(action);
+  });
+
+  it('should update the display with the counter value', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('div').textContent).toContain('Current Count: 0');
+
+    component.increment();
+
+    component.count$.subscribe(res => {
+      expect(res).toBe(1);
+    });
+
+  });
+
 });
