988c1ecc197ead4bb4bc1161638aeb4cf9589757
Prabu <meetprabu88@gmail.com>
2019-01-07 15:55:18 +0530

    Adds tests for http requests when saving counter

--- src/app/counter/counter.service.spec.ts
+++ src/app/counter/counter.service.spec.ts
@@ -1,15 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
-
+import { HttpClient } from '@angular/common/http';
+import { asyncData } from '../testing/async-observable-helpers';
 import { CounterService } from './counter.service';
 
 describe('CounterService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-  	imports: [HttpClientModule],
-  }));
+  let spyHttpService;
+  beforeEach(() => {
+    spyHttpService = jasmine.createSpyObj('HttpClient', ['post']);
+    TestBed.configureTestingModule({
+      providers: [{provide: HttpClient, useValue: spyHttpService}]
+		});
+	})
 
   it('should be created', () => {
     const service: CounterService = TestBed.get(CounterService);
     expect(service).toBeTruthy();
+  });
+
+  it('should save the counter value', () => {
+    const service: CounterService = TestBed.get(CounterService);
+    const responsePayload = {id: 1, counter: 42};
+    spyHttpService.post.and.returnValue(asyncData(responsePayload));
+
+    service.saveCounter(42).subscribe(res => expect(res).toEqual(responsePayload));
+    expect(spyHttpService.post).toHaveBeenCalled();
   });
 });
