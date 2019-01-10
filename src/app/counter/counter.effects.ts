3111cc958c2224cae6d2db07d4d69f84ed9f1837
Prabu <meetprabu88@gmail.com>
2019-01-10 19:01:44 +0530

    Fixes test by adding type to variable

--- src/app/counter/counter.effects.ts
+++ src/app/counter/counter.effects.ts
@@ -3,7 +3,7 @@ import {Effect, Actions, ofType} from '@ngrx/effects';
 import {Observable, of} from 'rxjs';
 import {exhaustMap, map, catchError} from 'rxjs/operators';
 
-import {ActionTypes, Increment, Decrement} from '../counter.actions';
+import {ActionTypes, Increment, Decrement, Save} from '../counter.actions';
 import {CounterService} from './counter.service';
 
 @Injectable()
@@ -13,7 +13,8 @@ export class CounterEffects {
   @Effect()
   saveCounter$ = this.actions$.pipe(
     ofType(ActionTypes.SAVE),
-    exhaustMap(action => this.counterService.saveCounter(action.payload).pipe(
+    map((action: Save) => action.payload),
+    exhaustMap((value) => this.counterService.saveCounter(value).pipe(
       map((res) => new Increment()),
       catchError(err => of(new Decrement())))
     )
